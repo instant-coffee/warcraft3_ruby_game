543596d2292458b945ef15dfafca1ce638d6a9db
instant-coffee <lachlanrm@gmail.com>
2015-03-10 19:51:15 +0000

    Completed

--- lib/footman.rb
+++ lib/footman.rb
@@ -1,19 +1,10 @@
 # http://classic.battle.net/war3/human/units/footman.shtml
 
 class Footman < Unit
-  attr_reader :health_points, :attack_power
-
+  
   def initialize
     @health_points = 60
     @attack_power = 10
-  end
-
-  def attack!(enemy)
-    enemy.damage(attack_power)
-  end
-
-  def damage(attack_power)
-    @health_points = @health_points -= attack_power
   end
 
 end
