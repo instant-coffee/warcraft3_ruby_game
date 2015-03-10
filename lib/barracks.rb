1d31bbdb89eceec26c271eb81ce91ee49a873c57
instant-coffee <lachlanrm@gmail.com>
2015-03-10 23:55:27 +0000

    Enhancements stage 4 w/ testing

--- lib/barracks.rb
+++ lib/barracks.rb
@@ -1,12 +1,14 @@
 class Barracks < Unit
 
-  def initialize
+  attr_reader :lumber
+
+  def initialize 
     @gold = 1000
     @food = 80
     @health_points = 500
+    @lumber = 500
   end
 
-##why does it work with a local variable??
   def can_train_footman?
     if (gold >= 135) && (food >= 2)
       @gold = @gold -= 135
@@ -25,7 +27,6 @@ class Barracks < Unit
     ((gold >= 90) && (food >= 5)) ? true : false
   end
 
-  ###??? why does it have to be in this order to pass?
   def train_peasant
     if can_train_peasant?
       @gold = @gold -= 90
