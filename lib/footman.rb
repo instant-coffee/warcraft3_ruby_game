a0902d28f4c6a073ff271fcfcd5c265126578818
instant-coffee <lachlanrm@gmail.com>
2015-03-10 19:31:27 +0000

    Peasants are in the mix

--- lib/footman.rb
+++ lib/footman.rb
@@ -1,11 +1,19 @@
 # http://classic.battle.net/war3/human/units/footman.shtml
 
-class Footman
+class Footman < Unit
   attr_reader :health_points, :attack_power
 
   def initialize
     @health_points = 60
     @attack_power = 10
+  end
+
+  def attack!(enemy)
+    enemy.damage(attack_power)
+  end
+
+  def damage(attack_power)
+    @health_points = @health_points -= attack_power
   end
 
 end
--- lib/unit.rb
+++ lib/unit.rb
@@ -1,10 +1,10 @@
 class Unit
 
-  attr_reader :health_points
+  attr_reader :health_points, :attack_power
 
   def initialize(health_points, attack_points)
     @health_points = health_points
-    @attack_points = attack_points
+    @attack_power = attack_power
   end
 
 end
